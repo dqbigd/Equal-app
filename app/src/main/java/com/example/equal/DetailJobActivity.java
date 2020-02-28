d1ae425c32c31f163a7536c4002dac77eb21975b
agung <agungwaskito2000@gmail.com>
2020-02-28 12:47:55 +0700

    layout detail_job

---
+++ app/src/main/java/com/example/equal/DetailJobActivity.java
@@ -0,0 +1,14 @@
+package com.example.equal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_job);
+    }
+}
